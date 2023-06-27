2d610df1008a28cccbd6fd4f608c10c7b41d36fe
Francisco <frandelosantoss12@gmail.com>
2023-06-27 10:40:14 -0300

    Set logo in header

--- client/src/components/header/header.component.tsx
+++ client/src/components/header/header.component.tsx
@@ -1,13 +1,22 @@
 import { CreateUser } from '@/components'
 
 import styles from './header.component.module.css'
+import logo from '@/assets/logo-dark.svg'
 
 type HeaderProps = { getUsersHandler: () => void }
 
 const Header = ({ getUsersHandler }: HeaderProps) => {
   return (
     <header className={styles.header}>
-      <h1 className={styles.headerTitle}>Users-manager</h1>
+      <a className={styles.headerLink} href="/">
+        <img
+          src={logo}
+          alt="Francisco De Los Santos Logo"
+          width={40}
+          height={40}
+        />
+        <h1 className={styles.headerTitle}>Users-manager</h1>
+      </a>
       <CreateUser onCreate={getUsersHandler} />
     </header>
   )
